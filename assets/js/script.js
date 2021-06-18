d2f60ca15817e4dc392ef7c1b6b2cb5c8fe9af9d
ElusiveEntity <30778577+ElusiveEntity@users.noreply.github.com>
2021-06-18 18:19:53 -0400

    Created class for quiz items, created placeholder question bank.

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,21 +3,55 @@ var quizContentEl = document.querySelector("#quiz-content");
 var quizFooterEl = document.querySelector("#quiz-footer");
 var buttonStartStop = document.querySelector("#startStopButton");
 
-//Function for creating a new question object.
-function QuizItem(question, correctA, wrongA1, wrongA2, wrongA3) {
-    this.question = question;
-    this.correctA = correctA;
-    this.wrongA1 = wrongA1;
-    this.wrongA2 = wrongA2;
-    this.wrongA3 = wrongA3;
+//class object for handling QuizItems
+class QuizItem {
+
+    //quizItem ctor
+    constructor(question, correctA, wrongA1, wrongA2, wrongA3) {
+        this.question = question;
+        this.correctA = correctA;
+        this.wrongA1 = wrongA1;
+        this.wrongA2 = wrongA2;
+        this.wrongA3 = wrongA3;
+    }
 }
 
+//Build up an array of QuizItems
 var questionBank = [
     new QuizItem(
-        "Which of the following are NOT primitive types?",
+        "Which of the following is NOT a primitive type?",
         "object",
         "boolean",
         "string",
         "number"
+    ),
+    new QuizItem(
+        "Placeholder question 1",
+        "correct answer",
+        "wrong answer 1",
+        "wrong answer 2",
+        "wrong answer 3"
+    ),
+    new QuizItem(
+        "Placeholder question 2",
+        "correct answer",
+        "wrong answer 1",
+        "wrong answer 2",
+        "wrong answer 3"
+    ),
+    new QuizItem(
+        "Placeholder question 3",
+        "correct answer",
+        "wrong answer 1",
+        "wrong answer 2",
+        "wrong answer 3"
+    ),
+    new QuizItem(
+        "Placeholder question 4",
+        "correct answer",
+        "wrong answer 1",
+        "wrong answer 2",
+        "wrong answer 3"
     )
 ];
+
